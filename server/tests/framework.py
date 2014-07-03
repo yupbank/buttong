f946499559763b5634120b113da6792433c577cd
yupbank <yupbank@gmail.com>
2014-07-03 17:02:58 +0800

    ok

---
+++ server/tests/framework.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+framework.py
+Author: yupbank
+Email:  yupbank@gmail.com
+
+Created on
+2014-07-03
+'''
+class BaseTestCase(object):
+    pass
---
+++ server/tests/test_wml.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+test.py
+Author: yupbank
+Email:  yupbank@gmail.com
+
+Created on
+2014-07-03
+'''
+import pytest
+from framework import BaseTestCase
+
+class TestWml(BaseTestCase):
+    def test_process(self):
+        
